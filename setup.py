3afcd42ea8b2e3618ce148abf9b49384e3485ecc
Xavier Codinas <xavier19966@gmail.com>
2019-11-23 14:14:34 +0100

    Change flask distribution

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
+        'Flask',
         'Flask-sqlalchemy',
         'Flask-migrate',
         'Flask-script',
