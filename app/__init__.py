af98665db1445695f0e22fedf83d09cd991edf0b
Xavier Codinas <xavier19966@gmail.com>
2019-11-23 20:13:54 +0100

    Remove sqlalchemy custom class to prevent openning connections

--- app/__init__.py
+++ app/__init__.py
@@ -5,19 +5,11 @@ from werkzeug.exceptions import HTTPException
 
 from flask import Flask
 from flask_restful import Api as _Api
-from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
 from config import Config
-
-
-class SQLAlchemy(_BaseSQLAlchemy):
-
-    # https://github.com/pallets/flask-sqlalchemy/issues/589#issuecomment-361075700
-    def apply_pool_defaults(self, app, options):
-        super(SQLAlchemy, self).apply_pool_defaults(app, options)
-        options["pool_pre_ping"] = True
 
 
 app = Flask(__name__)
